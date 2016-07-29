5241c53e549d14110b04efc63e1df95f67b86e95
Steve Bedell <bedell.369@osu.edu>
2016-07-29 12:32:12 -0400

    Adding toastr loading bars for the security page

--- public/js/security.controller.js
+++ public/js/security.controller.js
@@ -8,13 +8,27 @@ function securityController($scope, securityFactory) {
     $scope.currentResolution = `${window.innerWidth} x ${window.innerHeight}`;
     $scope.batteryInfo = navigator.battery ? `${navigator.battery.level * 100}%` : "None";
 
+    toastr.options = {
+        "progressBar": true,
+        "positionClass": "toast-top-center",
+        "showDuration": "1000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+
     $scope.searchIpAddr = function() {
         $scope.clearResults(false);
         var ipAddress = document.getElementById("ipaddr").value;
 
         if (ipAddress.match(/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)) {
+            toastr.info("Searching...");
             securityFactory.searchIpAddr(ipAddress).then(function(result) {
                 $scope.ipInfo = result;
+                toastr.clear();
             });
         } else {
             $scope.errorMessage = "Invalid IP (ipv4) address.";
@@ -26,8 +40,10 @@ function securityController($scope, securityFactory) {
         var port = document.getElementById("port").value;
         
         if (port.match(/^\d+$/) && port > 0 && port < 65536) {
+            toastr.info("Searching...");
             securityFactory.searchPort(port).then(function(result) {
                 $scope.portInfo = result;
+                toastr.clear();
             });
         } else {
             $scope.errorMessage = "Invalid port number.";
